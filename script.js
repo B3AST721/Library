d152dc0353b2515a08a904fcd3b4259a6c996c1f
Jaheel <jaheelwarr@gmail.com>
2022-03-08 18:48:29 -0600

    Card display working

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 //Stores the books.
-let myLibrary = [
-  {
-    name: 'book',
-    author: 'author',
-    page: 15
-  },
-];
+let myLibrary = [];
 
 //Constructor.
-function Book(name, author, page) {
-  this.name = name;
-  this.author = author;
-  this.page = page;
+class Book{
+  constructor(name, author, page){
+    this.name = name;
+    this.author = author;
+    this.page = page;
+  }
 }
-
 //Add the users input into the library array and then display it.
 function addBooks(name, author, page) {
   myLibrary.push(new Book(name, author, page))
 }
 
-function displayTheBooks() {
-    const title = document.querySelector('#name');
-    const writer = document.querySelector('#author');
-    const numPages = document.querySelector('#pages')
-    myLibrary.forEach((book) => {
-    title.innerHTML = (book.name)
-    writer.innerHTML = (book.author)
-    numPages.innerHTML = (book.page)
+const displayTheBooks = () => {
+  myLibrary.forEach((book) => {
+    const card = document.createElement('div');
+    const nameDisplay = document.createElement('div');
+    const authorDisplay = document.createElement('div');
+    const pageDisplay = document.createElement('div');
+
+    card.className = 'card';
+    nameDisplay.innerHTML = 'Name: ' + book.name;
+    authorDisplay.innerHTML = 'Author: ' + book.author;
+    pageDisplay.innerHTML = 'Page: ' + book.page;
+
+    document.body.appendChild(card)
+    card.appendChild(nameDisplay);
+    card.appendChild(authorDisplay);
+    card.appendChild(pageDisplay);
   })
 }
 
 addBooks('Trial', 'Trial', 54)
+addBooks('hw', 'hw', 20)
 
 displayTheBooks()
